d83ff5ebec6c21340363a1b3d78c736576008794
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2023-07-31 02:26:12 +0700

    validation request helper

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductController;
+use Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
