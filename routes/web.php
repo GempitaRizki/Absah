c4b7ced3ade3d9f2cbbb8682d68bc253cdd7cb60
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2023-07-31 02:25:42 +0700

    validation request helper

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,18 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'auth
     Route::get('dashboard', 'DashboardController@index')->name('dashboard');
 
     // Category
-    Route::get('categories', 'CategoryController@index')->name('categories.index');
-    Route::get('categories/create', 'CategoryController@create')->name('categories.create');
-    Route::post('categories', 'CategoryController@store')->name('categories.store');
-    Route::get('categories/{category}/edit', 'CategoryController@edit')->name('categories.edit');
-    Route::put('categories/{category}', 'CategoryController@update')->name('categories.update');
-    Route::delete('categories/{category}', 'CategoryController@destroy')->name('categories.destroy');
+    Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::put('categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 
     // Product
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('products/create', [ProductController::class, 'create'])->name('categories.create');
+    Route::post('products', [ProductController::class, 'store'])->name('categories.create');
+
 });
 
 
